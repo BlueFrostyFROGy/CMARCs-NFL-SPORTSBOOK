b6a6861b06935e14e001fe080eee4d5d47ac3399
Caden Marchetti <CadenArch@Marchetti-MacBook-Air.local>
2026-01-16 19:43:20 -0600

    auth: add fallback domain for CONVEX_SITE_URL

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL,
+      domain: process.env.CONVEX_SITE_URL ?? "https://cmarcsnflsportsbook.vercel.app",
       applicationID: "convex",
     },
   ],
